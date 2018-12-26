f0bd9fa7faa9f7d8e89846c327edd657f88b8064
asksven <sven.knispel@gmail.com>
2018-12-26 14:30:10 +0100

    Removed docker.io from image name

--- ops/deploy-minikube.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-
-#kubectl delete namespace codemelter
-
-kubectl --namespace=codemelter apply -f manifests/10-ns.yaml
-
-kubectl --namespace=codemelter apply -f manifests-minikube
